d2c4168d4c4a091922e6aca5258662f45a9d980d
PieGuy123 <joe.boudreau999@gmail.com>
2016-06-02 22:11:57 -0400

    Actually working input / filing the objects
    in this update, the 2D array of objects will actually work.

--- Apple_Colour.java
+++ Apple_Colour.java
@@ -6,28 +6,41 @@ import javax.imageio.ImageIO;
 
 public class Apple_Colour {
 
-	class apple{
+	static class apple{
+		//these are the three value that the 'apple' object has, the R stands for RED, G stands for Green, and B stands for Blue
 		int R = 0;
 		int G = 0;
 		int B = 0;
 		
+		//this is the constructor
+		public apple(){
+			R = 0;
+			G = 0;
+			B = 0;
+		}
+
+		
+		
+	/**
+	 * This method is used to set the RGB value for the object
+	 * @param R1
+	 * @param G1
+	 * @param B1
+	 */
 	public void setVariables(int R1, int G1, int B1){
 		R= R1;
 		G = G1;
 		B = B1;
-		System.out.println("worked");
+		//System.out.println("worked");
 	}
-	public void initialize(){
-		R = 0;
-		G = 0;
-		B = 0;
-	}
-	public apple(int SR, int SG, int SB){
-		R = SR;
-		G = SG;
-		B = SB;
+	/**
+	 * This method simply prints out the RGB values int eh object
+	 */
+	public void printout(){
+		System.out.println(R + "\n" + G + "\n" + B);
 	}
 	
+		
 	}
 	
 	
@@ -36,19 +49,21 @@ public class Apple_Colour {
 	
 	//@SuppressWarnings("null")		//not exactly sure what this does????
 	public static void main(String[] args) {
-		apple testThing = null;
-		testThing.initialize();
-		testThing.setVariables(0, 0, 0);
-		
-		
-		
 		
 		try{
 			String FileName = "red.png";
 			BufferedImage image = ImageIO.read(new File(FileName));
-			apple rgbArray[][] = new apple[image.getWidth()][image.getHeight()];
+			apple[][] rgbArray = new apple[image.getWidth()][image.getHeight()];
 			
-			//testThing.setVariables(0, 0, 0);
+			//this section of code will create the 2D array of objects (if you don't have this, you will get a Null pointer exception)
+			for(int i = 0; i < image.getWidth(); i++){
+				for(int j = 0; j < image.getHeight(); j++){
+					rgbArray[i][j] = new apple();
+				}
+				}
+			
+			
+
 			
 			
 			for(int i = 0; i < image.getWidth(); i++){
@@ -57,27 +72,26 @@ public class Apple_Colour {
 					
 					int red = mycolour.getRed();
 					int green = mycolour.getGreen();
-					 int blue = mycolour.getBlue();
-					//rgbArray[i][j].setR(temp); 
-					//testThing.setVariables(red, green, blue);
+					int blue = mycolour.getBlue();
 					
-					//rgbArray[i][j].R = mycolour.getRed();
-					//rgbArray[i][j].G = mycolour.getGreen();
-					//rgbArray[i][j].B = mycolour.getBlue();
+					rgbArray[i][j].setVariables(red,green,blue);
+					rgbArray[i][j].printout();
+					 
 					
-					
-					//System.out.println("R= " + mycolour.getRed());
-					//System.out.println("B= " + mycolour.getGreen());
-					//System.out.println("G= " + mycolour.getBlue());
 					
 				}
 			}
-			System.out.println(rgbArray[0][0].R);
+			//System.out.println(rgbArray[0][0].R);
 			
 		}
 		catch(IOException e){System.out.println("There was a problem");}
 		//System.out.println(rgbArray[0][0].R);
 	}
-
 }
+
+
+
+
+
+	
 
