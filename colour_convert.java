df4430e56f177acf94d4c1eab8529e9fb6de2402
PieGuy123 <joe.boudreau999@gmail.com>
2016-06-09 09:58:42 -0400

    Figure out what the colours are
    This should be able to take the RGB values, and determine what colour it is

---
+++ colour_convert.java
@@ -0,0 +1,13 @@
+
+public class colour_convert {
+	
+	
+	public static void main(String[] args) {
+		analysis_test.setVariables1(analysis_test.rgbArray, 99, 99, 99);
+		analysis_test.getR(analysis_test.rgbArray);
+		
+			
+
+	}
+
+}
